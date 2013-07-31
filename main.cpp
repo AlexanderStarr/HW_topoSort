557f8b6793bb4283e067e35863f9e8ec7e56068b
Alexander Starr <AlexanderLStarr@gmail.com>
2013-07-31 11:13:05 -0500

    Added documentation.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,23 @@
 
 using namespace std;
 
-// To represent a graph, you use an array of graphNodes.  successors is a
-// C++ list of the integer index of each node that the current node points to.
-// Thus, you can access any successor in constant time.
+// To represent a graph, you use an array of graphNodes.
+
+// ========== graphNode ========== //
 template <class T>
 struct graphNode {
 	T nodeID;
+	
+	// isAssigned keeps track of which nodes in an array have actually been
+	// given a value.  This is necessary because the array is initialized with
+	// 'empty' nodes.
 	bool isAssigned;
+	
+	// isExplored is used when doing the topological sorting.
 	bool isExplored;
+	
+	// successors is a C++ list of the integer index of each successor node.
+	// Thus, you can access any successor in constant time.
 	list<int> *successors;
 	graphNode(){isAssigned=false; isExplored=false; successors=new list<int>;};
 	void printNode();
@@ -34,10 +43,13 @@ void graphNode<T>::printNode() {
 	cout << ']';
 }
 
+// ========== Graph ========== //
 template <class T>
 class Graph {
 	int size;
 	graphNode<T> *arr;
+	
+	// order is used during topoSort
 	stack<int> order;
 public:
 	Graph(){};
@@ -66,10 +78,13 @@ void Graph<T>::printGraph() {
 	}
 }
 
+// Returns the index of the node with the given ID if found.
+// Otherwise, returns 0.
 template <class T>
 int Graph<T>::findNode(T search_ID) {
 	for (int i=0; i<size; i++) {
 		if (not arr[i].isAssigned) {
+			// If a node isn't assigned, then the array effectively ends there.
 			break;
 		}
 		if (arr[i].nodeID == search_ID) {
@@ -79,6 +94,8 @@ int Graph<T>::findNode(T search_ID) {
 	return -1;
 }
 
+// If a node with the given ID doesn't exist, it adds it and returns the index.
+// Otherwise, it returns the index of the node with the given ID.
 template <class T>
 int Graph<T>::addOrFindNode(T init_ID) {
 	for (int i=0; i<size; i++) {
@@ -94,6 +111,8 @@ int Graph<T>::addOrFindNode(T init_ID) {
 	return 0;
 }
 
+// Given the IDs of the predecessor and successor nodes, this adds the
+// arc to the graph.
 template <class T>
 void Graph<T>::addArc(T pre, T suc) {
 	int pre_loc = addOrFindNode(pre);
@@ -101,6 +120,11 @@ void Graph<T>::addArc(T pre, T suc) {
 	arr[pre_loc].addSuccessor(suc_loc);
 }
 
+// A depth-first search algorithm.  First it marks the current node as
+// explored.  Then it recursively performs a depth-first search on each
+// successor node.  Once all successors are searched, it pushes the node
+// to the order stack.  The end result of this is that it the deepest nodes
+// will be the first to the stack, and thus the last nodes out of the stack.
 template <class T>
 void Graph<T>::DFS(int index) {
 	arr[index].isExplored = true;
@@ -113,6 +137,8 @@ void Graph<T>::DFS(int index) {
 	order.push(index);
 };
 
+// Simply ensures that DFS reaches every node, then prints each node
+// from the order stack.
 template <class T>
 void Graph<T>::topoSort() {
 	for (int i=0; i<size; i++) {
@@ -126,12 +152,14 @@ void Graph<T>::topoSort() {
 	};
 };
 
+// Borrowed from the first programming assignment.  Returns a string
+// consisting of all characters from i to the next delimiter.
 string getNextNode(string raw_string, int i)
 {
 	string nodeID = "";
 	const string DELIM = " ,()";
 	while (i<raw_string.length()) {
-		// Iterate through equation until you hit a delimiter.
+		// Iterate through string until you hit a delimiter.
 		if (DELIM.find(raw_string[i]) != -1) {
 			break;
 		}
@@ -149,13 +177,23 @@ int main () {
 	int num;
 	cin >> num;
 	Graph<string> myGraph = Graph<string>(num);
+	
+	// We will build a predecessor and successor string, in that order.
+	// Once we have one of each, create an arc in the graph with them,
+	// then reset each one and start again.
 	string pre = "", suc = "";
 	const string DELIM = " ,()";
 	int i = 0;
 	while (i<raw_string.length()) {
+		// Simply ignore instances of the delimiter characters
 		if (DELIM.find(raw_string[i]) != -1) {
 			i++;
 		}
+		
+		// If a character is not a delimiter, grab the string until the next
+		// delimiter.  If there is not currently a predecessor or successor,
+		// then assign the string to pre.  Otherwise, if pre is assigned,
+		// then assign the string to suc, create the arc, and reset pre/suc.
 		else {
 			if (pre == "" && suc == "") {
 				pre = getNextNode(raw_string, i);
